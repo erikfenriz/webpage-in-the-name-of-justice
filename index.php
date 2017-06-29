28f5de7eca7a27f0ca1334a94ea14b79bbaae5ba
Erik Rybalkin <erikfenriz@gmail.com>
2017-06-29 16:56:09 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Personal Website</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.html">Main</a>
+                </li>
+                <li>
+                    <p id="symbol">&#969;</p>
+                </li>
+                <li><a href="library.html">Library</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <section>
+        <h1>Personal Website in the Name of Justice</h1>
+        <main>
+            <img id="avatar" src="0fmUL8cHywY.jpg" alt="Photo">
+            <p id="text">Good afternoon, noon, and some more. I am here to deploy, engage, initiate, and more cool phrases. I'd better use latin custom, huh?<br> I love photography and here I have uploaded some. Also, the HTML and CSS are all mine. Love.</p>
+        </main>
+        <footer>
+            <p id="copyright">Copyright &copy; <?php echo date("Y");>; Still Photography Credit: Sergey Kurzanov</p>
+        </footer>
+    </section>
+</body>
+
+</html>
